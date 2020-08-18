d6a3b4a975bf78b97021b74afa6aac184419d68d
imbdev <moose@inspiredmommyboss.com>
2020-08-18 14:22:24 +0800

    Search layout and UI fixed up  ... REST call begins now ... 18 Aug 2020

--- assets/src/js/_modules/SelflistSearch.js
+++ assets/src/js/_modules/SelflistSearch.js
@@ -5,6 +5,10 @@ class SelflistSearch {
     this.init();
     // COLLECTING BUTTON
     this.searchInput = $('#selflist-search-input');
+    this.searchResultBox = $('#selflist-search-result-box');
+    this.isSpinnerVisible = false;
+    this.previousValue;
+    this.typeTimer;
     this.setEvents();
   }
 
@@ -13,12 +17,34 @@ class SelflistSearch {
   };
 
   setEvents = () => {
-    this.searchInput.on('keyup', this.typingLogic);
+    this.searchInput.on('keyup', this.typeTimeLogic.bind(this));
   };
 
-  typingLogic(e) {
+  typeTimeLogic(e) {
     // console.log('clicked up from Sample Module ...');
+    if (this.searchInput.val() != this.previousValue) {
+      clearTimeout(this.typeTimer);
+
+      if (this.searchInput.val()) {
+        if (!this.isSpinnerVisible) {
+          this.searchResultBox.html('<div class="spinner-loader"></div>');
+          this.isSpinnerVisible = true;
+        }
+
+        this.typeTimer = setTimeout(this.getSearchResults.bind(this, e), 1000);
+      } else {
+        this.searchResultBox.html('');
+        this.isSpinnerVisible = false;
+      }
+    }
+
+    this.previousValue = this.searchInput.val();
+  }
+
+  getSearchResults(e) {
     console.log(`${e.keyCode} ... this key was pressed!`);
+    this.searchResultBox.html(`Ajax results go here ...`);
+    this.isSpinnerVisible = false;
   }
 }
 
--- template-list-search-index.php
+++ template-list-search-index.php
@@ -20,12 +20,14 @@ get_header('list');
 
   <section id="selflist-search-input-box" class="selflist-search-input-box">
 
-    <input type="text" id="selflist-search-input" class="selflist-search-input form-control">
+    <input type="text" id="selflist-search-input" class="selflist-search-input">
+    <i class="fas fa-search"></i>
 
   </section>
 
   <section id="selflist-search-result-box" class="selflist-search-result-box">
 
+    <!-- AJAX CONTENT GO HERE -->
     <article id="post-192" class="post-192 listings type-listings status-publish hentry">
       <header class="entry-header">
         <h2 class="entry-title">
@@ -36,46 +38,7 @@ get_header('list');
 
       <div class="entry-content">
         <p>
-          Private tutoring, Houston Heights location, Certified Teachers. 27 years experience in both Public and Private
-          schools. Payment cash $100 per 190 minute session. Bernadette Sweeny
 
-        </p>
-      </div><!-- .entry-content -->
-
-      <footer class="entry-footer">
-      </footer><!-- .entry-footer -->
-    </article>
-
-    <article id="post-192" class="post-192 listings type-listings status-publish hentry">
-      <header class="entry-header">
-        <h2 class="entry-title">
-          Tutoring Copy 2
-        </h2>
-      </header><!-- .entry-header -->
-
-
-      <div class="entry-content">
-        <p>
-          Private tutoring, Houston Heights location, Certified Teachers. 27 years experience in both Public and Private
-          schools. Payment cash $100 per 190 minute session. Bernadette Sweeny
-
-        </p>
-      </div><!-- .entry-content -->
-
-      <footer class="entry-footer">
-      </footer><!-- .entry-footer -->
-    </article>
-
-    <article id="post-192" class="post-192 listings type-listings status-publish hentry">
-      <header class="entry-header">
-        <h2 class="entry-title">
-          Tutoring Copy 3
-        </h2>
-      </header><!-- .entry-header -->
-
-
-      <div class="entry-content">
-        <p>
           Private tutoring, Houston Heights location, Certified Teachers. 27 years experience in both Public and Private
           schools. Payment cash $100 per 190 minute session. Bernadette Sweeny
 
